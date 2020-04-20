194292d896fc537f9c25b68190dfb5fb7c290074
Myugeli <myueyg@gmail.com>
2020-04-20 13:00:40 -0400

    Added convertAll function, fixed tagger

--- Util/vidUtil.py
+++ Util/vidUtil.py
@@ -18,14 +18,47 @@ def convertFormat(inputFile, outputFile):
         outputs={outputFile: None}
     )
     ff.run()
-#creates directory of tagged videos from an input csv file and an input video file 
+#converts all videos in input directory to avi video format in output directory    
+def convertAll(inputDir, outputDir):
+    inputVids = os.listdir(inputDir)
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)
+    outputDir = outputDir + "/"
+    inputDir = inputDir + "/"
+    for vid in inputVids:
+        vidStr = "".join(vid[0:len(vid) - 4:1]) + ".avi"
+        print(vidStr)       
+        convertFormat(inputDir + vid, outputDir + vidStr)
+    
+#returns {width, height} of input video as integers   
+def getResolution(inputVid):
+    vid = cv2.VideoCapture(inputVid)
+    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    return {width, height}
+
+#gets the fps of the input video
+def getFPS(inputVid):
+    vid = cv2.VideoCapture(inputVid)
+    fps = vid.get(cv2.CAP_PROP_FPS) 
+    return fps    
+
+#creates directory of tagged videos from an input csv file and an input video directory 
 #rows start at 1
 #get the sign label from column 2
 #end frames from column 9
 #start frames from column 8
-def tagger(inputFile, inputVid, outputDir, frameBufferValue, startRow, startCol, endCol, labelCol):
-    columns = defaultdict(list)
+#video number from column 5
+def tagger(inputFile, inputVidDir, outputDir):
     #amount of frames on either side
+    frameBufferValue = 20
+    startRow = 1
+    startCol = 8
+    endCol = 9
+    labelCol = 2
+    vidCol = 5
+    
+    columns = defaultdict(list)
     #open csv file
     rowCounter = 0
     with open(inputFile, newline = '') as csvfile:
@@ -33,16 +66,24 @@ def tagger(inputFile, inputVid, outputDir, frameBufferValue, startRow, startCol,
         for row in reader:
             for (i,v) in enumerate(row):
                 columns[i].append(v)
+            #get the video number in the row
+            vidNum = columns[vidCol][rowCounter]
+            #get the video number from the input video
+            vidString = "scene{num}-camera1.avi".format(num = vidNum)
+            inputVid = inputVidDir + '/'+ vidString
+            print(inputVid)
             #get the sign label from column 2
             if(rowCounter > startRow):
                 #get sign label
                 signLabel = columns[labelCol][rowCounter]   
+                
                 #get the frames that the label is associated with             
                 startFrame = int(columns[startCol][rowCounter]) - frameBufferValue              
                 endFrame = int(columns[endCol][rowCounter]) + frameBufferValue
                 #get frame rate of video
-                cap = cv2.VideoCapture(inputVid)
-                fps = cap.get(cv2.CAP_PROP_FPS)
+                fps = getFPS(inputVid)
+                if(fps == 0):
+                    break
                 #get timestamps using fps and frame numbers
                 startTime = startFrame/fps
                 endTime = endFrame/fps
@@ -50,8 +91,5 @@ def tagger(inputFile, inputVid, outputDir, frameBufferValue, startRow, startCol,
                 if not os.path.exists(outputDir):
                     os.makedirs(outputDir)
                 #create a new video file containing only the frames indicated
-                ffmpeg_extract_subclip(inputVid, startTime ,endTime, targetname= outputDir+ signLabel + ".avi")
-                print(str(rowCounter) + " " + signLabel + " " + str(startTime) + " " + str(endTime))
+                ffmpeg_extract_subclip(inputVid, startTime ,endTime, targetname= outputDir + '/'+ signLabel + ".avi")
             rowCounter += 1
-    
-        
